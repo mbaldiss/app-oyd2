14ccb325f69cb7aefec642fae69c121a6c3b605f
Marco Baldissone <marcol2c4@gmail.com>
2022-06-21 00:18:52 -0300

    v1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Models\Proceedings;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Gate;
 
 class HomeController extends Controller
 {
@@ -25,85 +27,137 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
+
+     // all
     public function index(){
         $orders = Order::all();
         $proceedings = Proceedings::all();    
         return view('home', ['orders' => $orders, 'proceedings' => $proceedings]);
     }
 
+    // solicitor
     public function add()
     {
-        return view('order/add');
+        $user = User::findOrFail(Auth::user()->id);
+        $response = Gate::inspect('add', $user);
+        if ($response->allowed()) {
+            return view('order/add');
+        } else {
+            echo $response->message();
+        }
     }
 
+    // solicitor
     public function save(Request $request){
-        $request->validate([
-            'description' => 'required|min:1',
-            'amount' => 'required|min:1',
-            'value' => 'required|min:1',
-            'type' => 'required|min:1'
-        ]);
-        $order = new Order;
-        $order->description = $request->description;
-        $order->amount = $request->amount;
-        $order->value = $request->value;
-        $order->type = $request->type;
-        $order->status = "GENERATED";
-        $order->save();
-        Session::flash('success', 'Order added correctly.');
-        return redirect( route('home') );
+        $user = User::findOrFail(Auth::user()->id);
+        $response = Gate::inspect('save', $user);
+        if ($response->allowed()) {
+            $request->validate([
+                'description' => 'required|min:1',
+                'amount' => 'required|min:1',
+                'value' => 'required|min:1',
+                'type' => 'required|min:1'
+            ]);
+            $order = new Order;
+            $order->description = $request->description;
+            $order->amount = $request->amount;
+            $order->value = $request->value;
+            $order->type = $request->type;
+            $order->status = "GENERATED";
+            $order->save();
+            Session::flash('success', 'Order added correctly.');
+            return redirect( route('home') );
+        } else {
+            echo $response->message();
+        }
     }
 
+    // solicitor
     public function delete($id){
-        $order = Order::find($id);
-        $order->delete();
-        return redirect( route('home') )->with('success', 'Order deleted successfully');
+        $user = User::findOrFail(Auth::user()->id);
+        $response = Gate::inspect('delete', $user);
+        if ($response->allowed()) {
+            $order = Order::find($id);
+            $order->delete();
+            return redirect( route('home') )->with('success', 'Order deleted successfully');
+        } else {
+            echo $response->message();
+        }
     }
 
+    // solicitor
     public function show($id){
-        $order = Order::find($id);
-        return view('order/show', ['order' => $order]);
+        $user = User::findOrFail(Auth::user()->id);
+        $response = Gate::inspect('show', $user);
+        if ($response->allowed()) {
+            $order = Order::find($id);
+            return view('order/show', ['order' => $order]);
+        } else {
+            echo $response->message();
+        }
     }
 
+    // solicitor
     public function update(Request $request, $id){
-        $request->validate([
-            'description' => 'required|min:1',
-            'amount' => 'required|min:1',
-            'value' => 'required|min:1',
-            'type' => 'required|min:1'
-        ]);
-        $order = Order::find($id);
-        $order->description = $request->description;
-        $order->amount = $request->amount;
-        $order->value = $request->value;
-        $order->type = $request->type;
-        $order->save();
-
-        return redirect( route('home') )->with('success', 'Order updated successfully');
+        $user = User::findOrFail(Auth::user()->id);
+        $response = Gate::inspect('update', $user);
+        if ($response->allowed()) {
+            $request->validate([
+                'description' => 'required|min:1',
+                'amount' => 'required|min:1',
+                'value' => 'required|min:1',
+                'type' => 'required|min:1'
+            ]);
+            $order = Order::find($id);
+            $order->description = $request->description;
+            $order->amount = $request->amount;
+            $order->value = $request->value;
+            $order->type = $request->type;
+            $order->save();
+            return redirect( route('home') )->with('success', 'Order updated successfully');
+        } else {
+            echo $response->message();
+        }
+        
     }
 
+    // authorizer
     public function create($id)
     {
-        $order = Order::find($id);
-        $order->status = "PROCEEDINGS";
-        $order->save();
-        
-        $proceedings = new Proceedings;
-        $proceedings->number = "000-" . $order->id;
-        $proceedings->user = Auth::user()->id;
-        $proceedings->order = $order->id;
-        $proceedings->save();
-        return redirect( route('home') )->with('success', 'Proceedings created correctly');
+        $user = User::findOrFail(Auth::user()->id);
+        $response = Gate::inspect('create', $user);
+        if ($response->allowed()) {
+            $order = Order::find($id);
+            $order->status = "PROCEEDINGS";
+            $order->save();
+            
+            $proceedings = new Proceedings;
+            $proceedings->number = "000-" . $order->id;
+            $proceedings->user = Auth::user()->id;
+            $proceedings->order = $order->id;
+            $proceedings->save();
+            return redirect( route('home') )->with('success', 'Proceedings created correctly');
+        } else {
+            echo $response->message();
+        }
     }
 
+    // authorizer
     public function finalize($id)
     {
-        $order = Order::find($id);
-        $order->status = "FINALIZED";
-        $order->save();
-        return redirect( route('home') )->with('success', 'Proceedings finalized correctly');
+        $user = User::findOrFail(Auth::user()->id);
+        $response = Gate::inspect('finalize', $user);
+        if ($response->allowed()) {
+            $order = Order::find($id);
+            $order->status = "FINALIZED";
+            $order->save();
+            return redirect( route('home') )->with('success', 'Proceedings finalized correctly');
+        } else {
+            echo $response->message();
+        }
     }
 
+    // all
     public function search($number){
         $proceedings = Proceedings::where('number', $number)->get();
         if($number == "000-"){
@@ -113,6 +167,7 @@ class HomeController extends Controller
         return view('home', ['orders' => $orders, 'proceedings' => $proceedings]);
     }
 
+    // all
     public function filter($status){
         if($status == "ALL"){
             $orders = Order::all();    
---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    // solicitor
+    public function add(User $user)
+    {
+        return $user->role == 'solicitor'
+                ? Response::allow()
+                : Response::deny('You do not have permission to perform this action.');
+    }
+    public function save(User $user)
+    {
+        return $user->role == 'solicitor'
+                ? Response::allow()
+                : Response::deny('You do not have permission to perform this action.');
+    }
+    public function delete(User $user)
+    {
+        return $user->role == 'solicitor'
+                ? Response::allow()
+                : Response::deny('You do not have permission to perform this action.');
+    }
+    public function show(User $user)
+    {
+        return $user->role == 'solicitor'
+                ? Response::allow()
+                : Response::deny('You do not have permission to perform this action.');
+    }
+    public function update(User $user)
+    {
+        return $user->role == 'solicitor'
+                ? Response::allow()
+                : Response::deny('You do not have permission to perform this action.');
+    }
+
+    // authorizer
+    public function create(User $user)
+    {
+        return $user->role == 'authorizer'
+                ? Response::allow()
+                : Response::deny('You do not have permission to perform this action.');
+    }
+    public function finalize(User $user)
+    {
+        return $user->role == 'authorizer'
+                ? Response::allow()
+                : Response::deny('You do not have permission to perform this action.');
+    }
+}
